0fd497dc591e242b580c38b8f79114a3b49cce74
RaeHoon Baek <bmp117@naver.com>
2022-08-11 17:54:53 +0900

    COVID19 예제 - 기능 구현하기 - 1

--- COVID19/ViewController.swift
+++ COVID19/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 import Charts
 
 class ViewController: UIViewController {
@@ -16,9 +17,44 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                debugPrint("success \(result)")
+            case let .failure(error):
+                debugPrint("success \(error)")
+            }
+        })
         
     }
 
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ) {
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey": "KFUYyduqOc3bDVSCRBx5TaJ9zvo2QtspP"
+        ]
+        
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                    
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+                
+            })
+    }
 
 }
 
